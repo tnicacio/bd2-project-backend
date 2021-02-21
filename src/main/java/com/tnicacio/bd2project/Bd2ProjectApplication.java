5959f543f4e9397a830b3d3d64bdb207951b55d9
tnicacio <nicacio.t92@gmail.com>
2021-02-21 10:47:14 -0300

    initial setup

---
+++ src/main/java/com/tnicacio/bd2project/Bd2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tnicacio.bd2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bd2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bd2ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tnicacio/bd2project/Bd2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnicacio.bd2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bd2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
